4cf19091af25169462d34d2e1e7b40f565f13dc6
Leroy <leroydeleeuw@gmail.com>
2019-01-08 20:15:46 +0100

    Added exceptions to be able to propagate errors.

---
+++ SpotifyAPI.Web/ApiRateExceededException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpotifyAPI.Web
+{
+    public class ApiRateExceededException : Exception
+    {
+        public ApiRateExceededException()
+            : base()
+        {
+
+        }
+    }
+}
---
+++ SpotifyAPI.Web/HttpException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpotifyAPI.Web
+{
+    public class HttpException : Exception
+    {
+        public HttpException(int code)
+            : base("Http error code " + code)
+        {
+
+        }
+    }
+}
